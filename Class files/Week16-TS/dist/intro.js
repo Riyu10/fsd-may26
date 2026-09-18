fabf56b6c59947dd482ae8d720e82543db9e49c7
Akshay Anand <akshay.anand.ar60@gmail.com>
2026-09-18 09:46:16 +0000

    week-16

---
+++ Class files/Week16-TS/dist/intro.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwenty(num) {
+    return num + 20;
+}
+const ab = addTwenty(10);
+const ba = addTwenty(10.4);
+console.log(ab);
+console.log(ba);
+//# sourceMappingURL=intro.js.map
---
+++ Class files/Week16-TS/src/intro.ts
@@ -0,0 +1,9 @@
+function addTwenty( num: number) : number{
+    return num + 20;
+}
+
+
+const ab = addTwenty(10);
+const ba = addTwenty(10.4)
+console.log(ab)
+console.log(ba)
